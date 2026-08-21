75e4dc0c4d55189331fb05691b0ae62df44cb1a8
Pedro-Vitor-Ribeiro-Silva <pedrovitorrbeirosilva@gmail.com>
2026-08-20 21:37:16 -0300

    feat: adiciona interatividade e responsividade

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+
   imports: [],
+
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+
+  menuAberto = false;
+
+  alternarMenu() {
+    this.menuAberto = !this.menuAberto;
+  }
 
 }
