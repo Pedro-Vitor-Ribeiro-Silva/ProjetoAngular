3a8a190d2af0b2ea5a65e2608944db600ad3eb19
Pedro-Vitor-Ribeiro-Silva <pedrovitorrbeirosilva@gmail.com>
2026-08-20 21:18:54 -0300

    feat: adiciona conteúdo ao header hero e footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+import { Header } from './components/header/header';
+import { Hero } from './components/hero/hero';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+
+  imports: [
+    Header,
+    Hero,
+    Footer
+  ],
+
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ProjetoAngular');
+
 }
